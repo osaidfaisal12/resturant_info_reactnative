7f944dd679ca242b99ed6f7895b104f963d088b8
osaidfaisal12 <osaidfaisalp@gmail.com>
2023-08-19 00:03:49 +0500

    hide key

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers:{
-        Authorization: 'bearer P9kPbunTAjzHipg3pDKkJpUDCGM5TaXwqbp6DJ-yFjT2f1d-QdDw7v5ozdoLvURJYQLy6pehRFSegzuBN6com8bHHQvuUUgtgVxl22xucgwpRrXSvZ67PVXgaI_bZHYx'
+        Authorization: 'bearer YOUR_API'
     }
 })
